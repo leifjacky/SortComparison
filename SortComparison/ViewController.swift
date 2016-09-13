27cf15a47c6fee63ef15b31c3440c9184d7407b1
leifjacky <leif.jacky@gmail.com>
2016-09-13 08:36:42 +0800

    Initial Commit

---
+++ SortComparison/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SortComparison
+//
+//  Created by Leif on 9/13/16.
+//  Copyright © 2016 Leif. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
